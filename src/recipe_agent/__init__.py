95b246a97d9544243583a7bfd254716ab16f9454
kuku-x <2335106344@qq.com>
2026-04-08 17:07:12 +0800

    refactor: migrate main.py and config.py to src/recipe_agent/

--- src/recipe_agent/__init__.py
+++ src/recipe_agent/__init__.py
@@ -1,5 +1,8 @@
-"""
-Recipe Agent - 基于图数据库的智能烹饪助手
-"""
+"""Recipe Agent - 基于图数据库的智能烹饪助手"""
 
 __version__ = "0.1.0"
+
+from .config import GraphRAGConfig, DEFAULT_CONFIG
+from .main import AdvancedGraphRAGSystem
+
+__all__ = ["GraphRAGConfig", "DEFAULT_CONFIG", "AdvancedGraphRAGSystem"]
